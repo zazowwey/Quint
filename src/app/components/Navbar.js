69d2ca75df38567da116134b1c1db7c13aadcf52
izzacodespace <zzxaa9391@gmail.com>
2026-05-28 15:15:17 +0700

    navbar fixing

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar({ isMenuOpen, setIsMenuOpen }){
     return(
         <nav style={{ opacity: 0, animationName: "slideDown", animationDuration: "1.2s", animationTimingFunction: "ease-out", animationFillMode: "forwards", animationDelay: "3.1s" }} className = "w-full h-auto bg-transparent fixed z-[999] top-0 bottom-auto right-0 left-0 backdrop-blur-md mix-blend-difference">
             <div className = "w-full h-full px-auto">
-                <div className = "w-full h-full flex flex-col items-center justify-between">
-                    <div className = "w-full h-full flex flex-row items-center justify-between py-[20px] px-(--main-padding) border-b border-solid border-(--C500) relative">
+                <div className = "w-full h-full flex flex-col items-center justify-between relative">
+                    <div className = "w-full h-full flex flex-row items-center justify-between py-[20px] border-b border-solid border-(--C500) relative">
                         
                         {/* Nav left col */}
-                         <div className = "lg:w-[50%] md:w-auto w-auto h-[40px] flex flex-row items-center justify-start gap-[83px]">
+                         <div className = "lg:w-[50%] md:w-auto w-auto h-[40px] flex flex-row items-center justify-start gap-[83px] pl-(--main-padding)">
                             
                             {/* Brand */}
                             <div className = "lg:w-[105px] md:w-[105px] w-auto h-auto p-[4px] overflow-hidden relative z-3 bg-white rounded-full flex items-center justify-start">
@@ -34,7 +34,7 @@ export default function Navbar({ isMenuOpen, setIsMenuOpen }){
 
                         {/* Nav right col */}
 
-                         <div className = "w-[50%] h-[40px] lg:flex md:hidden hidden flex-row items-center justify-end gap-[83px]">
+                         <div className = "w-[50%] h-[40px] lg:flex md:hidden hidden flex-row items-center justify-end gap-[83px] pr-(--main-padding)">
                             
                             {/* Brand name */}
 
@@ -63,14 +63,16 @@ export default function Navbar({ isMenuOpen, setIsMenuOpen }){
                             
                         </div>
 
-                    </div>
-                    {/* Menu wrapper */}
-                    <div className ="lg:w-[100%] md:w-auto sm:w-auto h-full lg:absolute md:static sm:static gap-[10px] px-(--main-padding) z-[2]"  onClick={() => setIsMenuOpen(!isMenuOpen)} >
-                        <div className = "flex flex-col items-center justify-center h-full w-full px-[16px] relative cursor-pointer gap-[6px]">
-                            <div className = "w-[32px] h-[2px] bg-white rounded-full"></div>
-                            <div className = "w-[32px] h-[2px] bg-white rounded-full "></div>
+                        {/* Menu wrapper */}
+                        <div className="lg:w-[100%] md:w-auto w-auto h-full lg:absolute md:static static gap-[10px] px-(--main-padding) z-[2]" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                            <div className = "flex flex-col items-center justify-center h-full w-full px-[16px] relative cursor-pointer gap-[6px]">
+                                <div className = "w-[32px] h-[2px] bg-white rounded-full"></div>
+                                <div className = "w-[32px] h-[2px] bg-white rounded-full "></div>
+                            </div>
                         </div>
+
                     </div>
+                    
 
 
 
